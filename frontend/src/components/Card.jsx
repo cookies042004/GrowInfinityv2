a6d4a35536e7d34b727082bf1a0526f01982151a
crazyshinobi <chetansingh410@gmail.com>
2024-11-16 15:02:49 +0530

    fix enquiry form

--- frontend/src/components/Card.jsx
+++ frontend/src/components/Card.jsx
@@ -169,7 +169,7 @@ export const Card = () => {
                   We will get back to you asap!
                 </p>
                 {/* {error && <Typography color="error">{error}</Typography>} */}
-                <form className="mx-8 mt-6">
+                <form className="mx-3 lg:mx-8 mt-6">
                   <div className="grid sm:grid-cols-12 gap-3">
                     <div className="col-span-6">
                       <div className="flex border rounded-lg items-center bg-white">
@@ -410,7 +410,7 @@ export const Card = () => {
                   We will get back to you asap!
                 </p>
                 {/* {error && <Typography color="error">{error}</Typography>} */}
-                <form className="mx-8 mt-6">
+                <form className="mx-3 lg:mx-8 mt-6">
                   <div className="grid sm:grid-cols-12 gap-3">
                     <div className="col-span-6">
                       <div className="flex border rounded-lg items-center bg-white">
@@ -651,7 +651,7 @@ export const Card = () => {
                   We will get back to you asap!
                 </p>
                 {/* {error && <Typography color="error">{error}</Typography>} */}
-                <form className="mx-8 mt-6">
+                <form className="mx-3 lg:mx-8 mt-6">
                   <div className="grid sm:grid-cols-12 gap-3">
                     <div className="col-span-6">
                       <div className="flex border rounded-lg items-center bg-white">
@@ -892,7 +892,7 @@ export const Card = () => {
                   We will get back to you asap!
                 </p>
                 {/* {error && <Typography color="error">{error}</Typography>} */}
-                <form className="mx-8 mt-6">
+                <form className="mx-3 lg:mx-8 mt-6">
                   <div className="grid sm:grid-cols-12 gap-3">
                     <div className="col-span-6">
                       <div className="flex border rounded-lg items-center bg-white">
@@ -1133,7 +1133,7 @@ export const Card = () => {
                   We will get back to you asap!
                 </p>
                 {/* {error && <Typography color="error">{error}</Typography>} */}
-                <form className="mx-8 mt-6">
+                <form className="mx-3 lg:mx-8 mt-6">
                   <div className="grid sm:grid-cols-12 gap-3">
                     <div className="col-span-6">
                       <div className="flex border rounded-lg items-center bg-white">
@@ -1374,7 +1374,7 @@ export const Card = () => {
                   We will get back to you asap!
                 </p>
                 {/* {error && <Typography color="error">{error}</Typography>} */}
-                <form className="mx-8 mt-6">
+                <form className="mx-3 lg:mx-8 mt-6">
                   <div className="grid sm:grid-cols-12 gap-3">
                     <div className="col-span-6">
                       <div className="flex border rounded-lg items-center bg-white">
@@ -1615,7 +1615,7 @@ export const Card = () => {
                   We will get back to you asap!
                 </p>
                 {/* {error && <Typography color="error">{error}</Typography>} */}
-                <form className="mx-8 mt-6">
+                <form className="mx-3 lg:mx-8 mt-6">
                   <div className="grid sm:grid-cols-12 gap-3">
                     <div className="col-span-6">
                       <div className="flex border rounded-lg items-center bg-white">
@@ -1772,7 +1772,247 @@ export const Card = () => {
             </Button>
           </div>
         </div>
-        
+        <div className="border relative p-2 hover:shadow-2xl bg-white">
+          <div className="flex justify-center">
+            <img src={propertycard} alt="" className="h-[230px] w-[100%]" />
+          </div>
+          <div className="mt-3 font-roboto text-sm flex justify-between">
+            <p className="font-semibold ps-3 text-sm">
+              Skyper Pool Apartment <br />{" "}
+              <span className="text-gray-700 font-normal text-xs">
+                By Max Estates
+              </span>
+            </p>
+            <p className="font-semibold text-[#EB664E] text-lg">₹2.8Cr*</p>
+          </div>
+          <div className="flex items-center mt-3">
+            <LocationOnIcon
+              sx={{
+                color: "darkblue",
+                fontSize: "18px",
+                paddingBottom: "3px",
+              }}
+            />
+            <p className="text-xs">1020 Bloomingdale Ave</p>
+          </div>
+          <div className="flex gap-3 justify-between mt-5 font-roboto">
+            <div className="flex gap-1 items-center">
+              <HomeIcon sx={{ color: "darkblue", fontSize: "18px" }} />
+              <p className="text-xs">4BHK</p>
+            </div>
+            <div className="flex gap-1 items-center">
+              <SquareFootIcon sx={{ color: "darkblue", fontSize: "18px" }} />
+              <p className="text-xs">1800 sq.ft</p>
+            </div>
+            <div className="flex gap-1 items-center">
+              <CurrencyRupeeIcon sx={{ color: "darkblue", fontSize: "18px" }} />
+              <p className="text-xs">10.6k / sq.ft.</p>
+            </div>
+          </div>
+
+          <div className="mt-5 flex gap-3">
+            <div className="flex-1">
+              <Link to="/project/abc">
+                <Button
+                  fullWidth
+                  variant="outlined"
+                  sx={{
+                    backgroundColor: "#03002e",
+                    color: "#fff",
+                    textTransform: "none",
+                  }}
+                >
+                  View Details
+                </Button>
+              </Link>
+            </div>
+            <div className="flex-1">
+              <Button
+                onClick={handleOpen}
+                fullWidth
+                variant="contained"
+                color="success"
+                startIcon={<CallIcon />}
+                sx={{ color: "#fff", textTransform: "none" }}
+              >
+                Enquiry
+              </Button>
+            </div>
+
+            <Modal
+              open={open}
+              onClose={handleClose}
+              aria-labelledby="modal-modal-title"
+              aria-describedby="modal-modal-description"
+              sx={{
+                borderRadius: "40px",
+              }}
+            >
+              <Box sx={style}>
+                <h1 className="font-roboto text-center text-2xl lg:text-4xl py-2">
+                  Contact Us
+                </h1>
+                <p className="font-roboto text-center text-sm lg:text-lg">
+                  We will get back to you asap!
+                </p>
+                {/* {error && <Typography color="error">{error}</Typography>} */}
+                <form className="mx-3 lg:mx-8 mt-6">
+                  <div className="grid sm:grid-cols-12 gap-3">
+                    <div className="col-span-6">
+                      <div className="flex border rounded-lg items-center bg-white">
+                        <div className="flex justify-center ps-3">
+                          <PersonIcon size="large" sx={{ color: "gray" }} />
+                        </div>
+                        <input
+                          type="text"
+                          className="outline-none p-3 rounded-lg w-full"
+                          placeholder="First Name"
+                        />
+                      </div>
+                    </div>
+                    <div className="col-span-6">
+                      <div className="flex border rounded-lg items-center bg-white">
+                        <input
+                          type="text"
+                          className="outline-none p-3 rounded-lg w-full"
+                          placeholder="Last Name"
+                        />
+                      </div>
+                    </div>
+                    <div className="col-span-12">
+                      <div className="flex border rounded-lg items-center bg-white">
+                        <div className="flex justify-center ps-3">
+                          <MarkunreadIcon sx={{ color: "gray" }} />
+                        </div>
+
+                        <input
+                          type="text"
+                          className="outline-none p-3 rounded-lg w-full"
+                          placeholder="Email Address"
+                        />
+                      </div>
+                    </div>
+                    <div className="col-span-12">
+                      <div className="flex border rounded-lg items-center bg-white">
+                        <div className="flex justify-center ps-3">
+                          <LocalPhoneIcon sx={{ color: "gray" }} />
+                        </div>
+                        <input
+                          type="text"
+                          className="outline-none p-3 rounded-lg w-full"
+                          placeholder="Phone"
+                        />
+                      </div>
+                    </div>
+                    <div className="col-span-12 lg:col-span-6">
+                      <FormLabel
+                        size="small"
+                        id="demo-row-radio-buttons-group-label"
+                        sx={{ fontWeight: "bold" }}
+                      >
+                        Your reason to buy is?
+                      </FormLabel>
+                      <RadioGroup
+                        row
+                        aria-labelledby="demo-row-radio-buttons-group-label"
+                        name="row-radio-buttons-group"
+                        size="small"
+                      >
+                        <FormControlLabel
+                          size="small"
+                          value="investment"
+                          control={<Radio size="small" />}
+                          label="Investment"
+                        />
+                        <FormControlLabel
+                          value="business"
+                          size="small"
+                          control={<Radio size="small" />}
+                          label="Business use"
+                        />
+                      </RadioGroup>
+                    </div>
+                    <div className="col-span-12 lg:col-span-6">
+                      <FormLabel
+                        id="demo-row-radio-buttons-group-label"
+                        sx={{ fontWeight: "bold" }}
+                      >
+                        Are you a property dealer?
+                      </FormLabel>
+                      <RadioGroup
+                        row
+                        aria-labelledby="demo-row-radio-buttons-group-label"
+                        name="row-radio-buttons-group"
+                        size="small"
+                      >
+                        <FormControlLabel
+                          value="yes"
+                          control={<Radio size="small" />}
+                          label="Yes"
+                          size="small"
+                        />
+                        <FormControlLabel
+                          size="small"
+                          value="no"
+                          control={<Radio size="small" />}
+                          label="No"
+                        />
+                      </RadioGroup>
+                    </div>
+                    <div className="col-span-12">
+                      <FormControlLabel
+                        control={<Checkbox size="small" defaultChecked />}
+                        label={
+                          <Typography
+                            variant="body2"
+                            sx={{ fontSize: "0.78rem" }}
+                          >
+                            I agree to be contacted by Grow infinity agents via
+                            whatsapp, SMS, Call, Email etc.
+                          </Typography>
+                        }
+                      />
+                    </div>
+                    <input
+                      type="hidden"
+                      name="propertyName"
+                      // value={formData.propertyName}
+                    />
+                    <div className="col-span-12">
+                      <Button
+                        variant="contained"
+                        sx={{
+                          background: "#03002E",
+                          textTransform: "none",
+                          padding: { xs: "5px", sm: "5px", md: "10px" },
+                          marginTop: "5px",
+                          marginBottom: "15px",
+                        }}
+                        size="large"
+                        fullWidth
+                        type="submit"
+                      >
+                        Send
+                      </Button>
+                    </div>
+                  </div>
+                </form>
+              </Box>
+            </Modal>
+          </div>
+
+          <div className="absolute top-[20px]">
+            <Button
+              endIcon={<DoneIcon />}
+              size="small"
+              variant="contained"
+              color="success"
+              sx={{ borderRadius: "0px", height: "25px" }}
+            >
+              RERA
+            </Button>
+          </div>
+        </div>
       </Slider>
     </div>
   );
