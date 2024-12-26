7e833870548c0259c635fd9cd991dedd2724ac50
crazyshinobi <chetansingh410@gmail.com>
2024-12-26 09:04:27 +0530

    update maintenance

--- frontend/src/components/UnderMaintenance.jsx
+++ frontend/src/components/UnderMaintenance.jsx
@@ -1,41 +1,14 @@
 import React from "react";
 
 export const UnderMaintenance = () => {
-  // Inline styles for the Under Maintenance page
-  const styles = {
-    container: {
-      display: "flex",
-      flexDirection: "column",
-      justifyContent: "center",
-      alignItems: "center",
-      height: "100vh",
-      backgroundColor: "#f8f9fa",
-      color: "#343a40",
-      textAlign: "center",
-      fontFamily: "Arial, sans-serif",
-    },
-    heading: {
-      fontSize: "3rem",
-      marginBottom: "1rem",
-    },
-    message: {
-      fontSize: "1.25rem",
-      marginBottom: "0.5rem",
-    },
-    signature: {
-      fontSize: "1rem",
-      color: "#6c757d",
-    },
-  };
-
   return (
-    <div style={styles.container}>
-      <h1 style={styles.heading}>We'll Be Back Soon!</h1>
-      <p style={styles.message}>
+    <div className="flex h-screen items-center justify-center flex-col gap-5" style={{ fontFamily: "Arial, sans-serif"}}>
+      <h1 className="text-xl lg:text-5xl">We'll Be Back Soon!</h1>
+      <p className="text-[14px] text-center lg:text-lg px-5">
         Our website is currently undergoing scheduled maintenance. We appreciate
         your patience.
       </p>
-      <p style={styles.signature}>- The Team</p>
+      <p className="text-sm text-[#6c757d] lg:text-lg">- The Team</p>
     </div>
   );
 };
